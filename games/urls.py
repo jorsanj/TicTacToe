f1a6b8440ec3e23999b8b30e510e720021b8a78b
Jorge San Jose <sanjoselorzajorge@gmail.com>
2023-08-27 17:13:02 +0200

    added create_game view and basic functionality

--- games/urls.py
+++ games/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_game/', views.create_game, name='create_game'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from games.models import Player
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+def create_game(request):
+    players = Player.objects.all()
+    context = {'players': players}
+
+    return render(request, 'create_game.html', context)
