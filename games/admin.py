f63ec539075f7731f21c819cc0354b5a1fbb6c96
Jorge San Jose <sanjoselorzajorge@gmail.com>
2023-08-27 17:09:57 +0200

    added database and changes in admin.py to being able to create players

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Player, Game
+
+
 # Register your models here.
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('player1', 'player2', 'winner', 'status')
+
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Game, GameAdmin)
