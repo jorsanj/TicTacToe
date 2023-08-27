2046b47e0d9f709900a5cb885995f26c1248092f
Jorge San Jose <sanjoselorzajorge@gmail.com>
2023-08-27 17:20:59 +0200

    game creation and first view of play game

--- games/urls.py
+++ games/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_game/', views.create_game, name='create_game'),
+    path('play_game/<int:game_id>/', views.play_game, name='play_game'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-from games.models import Player
+from games.models import Player, Game
 
 
 # Create your views here.
@@ -9,7 +10,30 @@ def index(request):
 
 
 def create_game(request):
+    if request.method == 'POST':
+        player1_id = request.POST.get('player1')
+        player1 = Player.objects.get(pk=player1_id)
+
+        player2_id = request.POST.get('player2')
+        player2 = Player.objects.get(pk=player2_id)
+
+        game = Game.objects.create(player1=player1, player2=player2)
+
+        return redirect('play_game', game_id=game.id)
+
     players = Player.objects.all()
     context = {'players': players}
 
     return render(request, 'create_game.html', context)
+
+
+def play_game(request, game_id):
+    game = Game.objects.get(id=game_id)
+
+    if game.status == 1:
+        return HttpResponse('Esta partida ya finalizó.')
+
+    current_player = 'player1' if game.board.count('X') == game.board.count('X') else player2
+    context = {'game': game, 'current_player': current_player}
+
+    return render(request, 'play_game.html', context)
